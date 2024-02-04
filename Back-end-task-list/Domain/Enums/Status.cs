d95b077ee9206184376b5ee3d524479d337a60f3
Tuanemendes <tuane.belmiro@gmail.com>
2024-02-04 17:04:18 -0300

    feat: create enum status

---
+++ Back-end-task-list/Domain/Enums/Status.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_end_task_list.Domain.Enums
+{
+    public enum Status
+    {
+        Pendente,
+        EmAndamento,
+        Concluido
+
+    }
+}
